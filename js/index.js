fcb1094e877e41ee7e8786da45018cad89ff250c
leroyg <leroy.gardner@gmail.com>
2019-05-03 13:36:09 -0500

    Checking git workflow

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
-// Your code goes here
+// * [ ] `mouseover`
+// * [ ] `keydown`
+// * [ ] `wheel`
+// * [ ] `drag / drop`
+// * [ ] `load`
+// * [X] `focus`
+// * [ ] `resize`
+// * [ ] `scroll`
+// * [ ] `select`
+// * [ ] `dblclick`
+
+// const lambdaBtn = document.querySelector('.lambda-button');
+// console.log(lambdaBtn);
+// lambdaBtn.addEventListener('click', function(event) {
+//   console.log('Work With Us clicked!');
+//   servicesHeader.textContent = 'Hello World';
+//   console.log(event);
+// });// Your code goes here
+
+const headerHover = document.querySelector('nav');
+
+console.log(headerHover);
+
+headerHover.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'red';
+});
+
+// headerHover.addEventListener('focus', (event) => {
+//     event.target.style.color = 'grey';
+// });
+
+
+// const password = document.querySelector('input[type="password"]');
+
+// password.addEventListener('focus', (event) => {
+//   event.target.style.background = 'pink';    
+// });
+
+// password.addEventListener('blur', (event) => {
+//   event.target.style.background = '';    
+// });
